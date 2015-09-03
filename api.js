18292074f6ab847dd89068fefb98988e9b862542
Jarom McDonald <jlmcdonald@gmail.com>
2015-09-03 14:49:54 -0600

    API page serves up static content
    Falcor router talking to data source properly

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config')[env];
+var bunyan = require('bunyan');
 var restify = require('restify');
 var pmongo = require('promised-mongo');
 var restifyMiddleware = require('falcor-restify');
@@ -11,6 +12,12 @@ var creds=config.database.user+":"+config.database.passwd;
 var conn_uri='mongodb://'+creds+'@'+config.database.host+':'+config.database.port+'/'+config.database.db;
 var db=pmongo(conn_uri,['students','applications']);
 
+var getAll = function(coll) {
+  return db[coll].find().toArray().then(function(documents) {
+    var packet=JSON.stringify(documents);
+  });
+};
+
 var router = new Router([{
   route: "applications",
   get: function() {
@@ -20,12 +27,6 @@ var router = new Router([{
     };
   }
 }]);
-
-var getAll = function(coll) {
-  db[coll].find().toArray().then(function(documents) {
-    var packet=JSON.stringify(documents);
-  });
-};
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -37,4 +38,9 @@ server.listen(config.server.port,function() {
 
 server.get('/model.json', restifyMiddleware(function (req, res, next) {
     return router;
+}));
+
+server.get(/\/.*/, restify.serveStatic({
+    directory: __dirname,
+    default: 'index.html'
 }));
