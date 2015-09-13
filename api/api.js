c89f35c560d0cfa6bc37d691aee1e013bf9bffa4
Jarom McDonald <jlmcdonald@gmail.com>
2015-09-13 00:59:04 -0600

    Added in Polymer front end (based on Polymer Starter Kit), framework for falcor-client element.

--- api/api.js
+++ api/api.js
@@ -55,6 +55,25 @@ var router = new Router([{
       });
     }
   }
+},
+{
+  route: "applications[{keys}]",
+  get: function(pathSet) {
+    if (typeof pathSet[1][0] === "undefined") {
+      return Application.find().exec().then(function(documents) {
+        return {"path":["applications"],"value":JSON.stringify(documents)};
+      });
+    }
+    else {
+      return Application.find({ 'netid': {$in: pathSet[1] }}).exec().then(function(documents) {
+        var results=[];
+        documents.forEach(function(document) {
+          results.push({"path":[pathSet[0],document.netid],"value":JSON.stringify(document)});
+        });
+        return results;
+      });
+    }
+  }
 }]);
 
 server.use(restify.acceptParser(server.acceptable));
