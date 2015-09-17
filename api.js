a01f18aebde4b7c7d17b899ab829eebc50430b59
Jarom McDonald <jlmcdonald@gmail.com>
2015-09-17 00:20:35 -0600

    moved routes to their own file.
    created session model, started generating functions.

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config')[env];
 var auth = require('./auth');
+var router = require('./router');
 
 var fs= require('fs');
 var bunyan = require('bunyan');
@@ -8,9 +9,6 @@ var restify = require('restify');
 var restifyMiddleware = require('falcor-restify');
 var falcor = require('falcor');
 var mongoose = require('mongoose');
-var Router = require('falcor-router');
-var Student = require('./models/student');
-var Application = require('./models/application');
 
 var LOG = bunyan.createLogger({
     name: 'demo',
@@ -41,61 +39,7 @@ var server = restify.createServer({
 
 var creds=config.database.user+":"+config.database.passwd;
 var conn_uri='mongodb://'+creds+'@'+config.database.host+':'+config.database.port+'/'+config.database.db;
-var db=mongoose.connect(conn_uri,['students','applications']);
-  
-var router = new Router([{
-  route: "credentials[{keys}]",
-  get: function(pathSet) {
-    return cas.getTicket(pathSet[1],pathSet[2], config.url)
-    .then(function(ticket){
-      return cas.validate(ticket, config.url);
-    })
-    .then(function success(response) {
-      return {"path":[response.username],"value":response.attributes};
-    })
-    .catch(function error(e) {
-      console.log("Invalid ticket. Error message was: " + e.message);
-    });
-  }
-},
-{
-  route: "students[{keys}]",
-  get: function(pathSet) {
-    if (typeof pathSet[1][0] === "undefined") {
-      return Student.find().exec().then(function(documents) {
-        return {"path":["students"],"value":JSON.stringify(documents)};
-      });
-    }
-    else {
-      return Student.find({ 'netid': {$in: pathSet[1] }}).exec().then(function(documents) {
-        var results=[];
-        documents.forEach(function(document) {
-          results.push({"path":[pathSet[0],document.netid],"value":JSON.stringify(document)});
-        });
-        return results;
-      });
-    }
-  }
-},
-{
-  route: "applications[{keys}]",
-  get: function(pathSet) {
-    if (typeof pathSet[1][0] === "undefined") {
-      return Application.find().exec().then(function(documents) {
-        return {"path":["applications"],"value":JSON.stringify(documents)};
-      });
-    }
-    else {
-      return Application.find({ 'netid': {$in: pathSet[1] }}).exec().then(function(documents) {
-        var results=[];
-        documents.forEach(function(document) {
-          results.push({"path":[pathSet[0],document.netid],"value":JSON.stringify(document)});
-        });
-        return results;
-      });
-    }
-  }
-}]);
+var db=mongoose.connect(conn_uri,['students','applications','sessions']);
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.bodyParser());
--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 var config = require('./config')[env];
 var cas = require('byu-cas');
 var service = config.url;
+var Session = require('./models/session');
 
 auth= {
   cas_server: "https://byu.edu/cas/",
@@ -13,6 +14,19 @@ auth= {
     .catch(function error(e) {
       console.log("Invalid ticket. Error message was: " + e.message);
     });
+  },
+  getSession:function(token) {
+    if (token) {
+      return Session.find({ 'session_id': token }}).then(function(session) {
+        return session;
+      });
+    }
+    else {
+      s = new Session({'session_id:','netid':''});
+      return s.save().then(function(session) {
+        return session;
+      })
+    }
   }
 };
 
