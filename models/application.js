f00a5069d22992958a496b3140c1a9b61d946add
Jarom McDonald <jlmcdonald@gmail.com>
2015-11-23 16:23:00 -0700

    Working app submission, working student documents in DB

--- models/application.js
+++ models/application.js
@@ -2,13 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var applicationSchema = new Schema({
-  coordinator_email: String,
-  coordinator_name: String,
-  numericSemester: Number,
+  coordinator_email: {type: String, required: true},
+  coordinator_name: {type: String, required: true},
+  numericSemester: {type: Number, required: true},
   permission: Boolean,
-  program: String,
-  semester: String,
-  year: Number
+  program: {type: String, required: true},
+  semester: {type: String, required: true},
+  year: {type: Number, required: true},
+  netid: {type: String, required: true},
+  fullname: String,
+  created_at: {type: Date, default: Date.now},
 });
 
 var Application = mongoose.model('application', applicationSchema);
