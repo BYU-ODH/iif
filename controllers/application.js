bfdf3b1350e54402c961da8a1a5753e9148cfb9c
jlmcdonald <jlmcdonald@gmail.com>
2016-01-21 05:03:47 +0000

    Ask for email from form rather than use the one from CAS

--- controllers/application.js
+++ controllers/application.js
@@ -38,7 +38,6 @@ exports.createApplication = function(req, res, next) {
         packet.netid = req.session_state.netid;
         packet.byuid = req.session_state.byuid;
         packet.fullname = req.session_state.student.fullname;
-        packet.email = req.session_state.student.email;
         var confmessage="An application for internship funding for the BYU college of Humanities was recently submitted. Here are the details.\n\n";
         for (var att in packet) {
           confmessage+=att+": "+packet[att]+"\n";
